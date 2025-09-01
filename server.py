639636c4aed6ab6e3fb45cbac6213a85fcd5ca42
dfernandesbsolus <96054351+dfernandesbsolus@users.noreply.github.com>
2025-09-01 14:49:16 +0200

    Update server.py

--- server.py
+++ server.py
@@ -174,7 +174,7 @@ def upload_to_s3(file_path: str, script_id: str, audio_id: str, file_extension: 
         )
         
         # Retornar URL do objeto
-        s3_url = f"https://s3.tebi.io/{script_id}/audio/{audio_id}.{file_extension}"
+        s3_url = f"https://s3.tebi.io/{bucket_name}/{script_id}/audio/{audio_id}.{file_extension}"
         logger.info(f"✅ Arquivo enviado para S3 : {s3_url}")
         return s3_url
     except Exception as e:
